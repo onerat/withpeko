355e0f09f3755fa5955dec3af85554b8777443ee
onerat <37900919+onerat@users.noreply.github.com>
2022-03-30 02:00:15 +0900

    add greedy

--- greedy.py
+++ greedy.py
@@ -1,1 +1,19 @@
 money = int(input())
+count = 0
+
+count += money // 50000
+money %= 50000
+count += money // 10000
+money %= 10000
+count += money // 5000
+money %= 5000
+count += money // 1000
+money %= 1000
+count += money // 500
+money %= 500
+count += money // 100
+money %= 100
+count += money // 50
+money %= 50
+
+print(f"answer = {count}")
